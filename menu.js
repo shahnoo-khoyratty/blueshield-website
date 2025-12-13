0e0d81d19ee326f1d635025ea852fc638819aff6
Shahnoo Khoyratty <shahnookhoyratty@gmail.com>
2025-12-14 03:21:15 +0400

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,11 @@
+﻿// Mobile burger menu toggle
+document.addEventListener('DOMContentLoaded', function() {
+  const burger = document.querySelector('.burger');
+  const navMenu = document.querySelector('.nav-menu');
+
+  if (burger && navMenu) {
+    burger.addEventListener('click', function() {
+      navMenu.classList.toggle('show');
+    });
+  }
+});

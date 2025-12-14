3bdcfe6fd438354e7152965433bf131b19ba2a50
Shahnoo Khoyratty <shahnookhoyratty@gmail.com>
2025-12-14 17:15:54 +0400

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,14 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-
     const burger = document.querySelector(".burger");
     const nav = document.querySelector("nav");
     const dropdowns = document.querySelectorAll(".dropdown");
     const cards = document.querySelectorAll(".card");
 
+    // Toggle mobile menu
     burger.addEventListener("click", () => {
         nav.classList.toggle("open");
     });
 
+    // Toggle dropdowns in mobile menu
     dropdowns.forEach(drop => {
         drop.querySelector("a").addEventListener("click", e => {
             e.preventDefault();
@@ -16,10 +17,11 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
+    // Toggle card popups on mobile
     cards.forEach(card => {
         card.addEventListener("click", () => {
             card.classList.toggle("open");
         });
     });
-
 });
+	

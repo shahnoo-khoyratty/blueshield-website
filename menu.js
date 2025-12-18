543745accb3c5111c41509eaf9c4d9cc9ab8875f
Shahnoo Khoyratty <shahnookhoyratty@gmail.com>
2025-12-18 13:10:01 +0400

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,19 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector("nav");
+  const dropdowns = document.querySelectorAll(".dropdown");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("open");
+  });
+
+  dropdowns.forEach(drop => {
+    const link = drop.querySelector("a");
+    link.addEventListener("click", e => {
+      if (window.innerWidth <= 768) {
+        e.preventDefault();
+        drop.classList.toggle("open");
+      }
+    });
+  });
+});
